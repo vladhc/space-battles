62f6d442d375dae852c566e4f4ff620d54e0efd3
Rasmus Buchmann <rasmus.buchmann@freiheit.com>
2020-04-16 22:58:36 +0200

    Starting the setup for the new environment

---
+++ ai/environment.py
@@ -0,0 +1,94 @@
+import numpy as np
+import json
+import gym
+from gym.spaces import Discrete, Box, Dict, Tuple
+
+import ray
+
+from game_client import RPSClient
+
+MAX_PLANETS = 21
+
+class SpaceEnv(gym.Env):
+    action_space = Tuple([
+        Discrete(MAX_PLANETS),  # from
+        Discrete(MAX_PLANETS),  # to
+        Box(low=0, high=1, shape=(3,), dtype=np.float32),  # ships(x3)
+    ])
+
+    def __init__(self, **kwargs):
+        self._client = None
+        self._last_game_state = None
+        self._last_planets_count = 1
+
+    def _lazy_init(self):
+        if self._client:
+            return
+        self._client = RPSClient(**kwargs)
+
+    def reset(self):
+        self._lazy_init()
+
+        self._client.reset()
+
+    def step(self, action):
+        planets = self._last_game_state['planets']
+
+        cmd = to_game_action(planets, action)
+        self._client.action(cmd)
+        state, reward, done = self._fetch()
+        return state, reward, done, {}
+
+    def _fetch(self):
+        game_state = self._client.game_state()
+        self._last_game_state = game_state
+        state = parse_game_state(game_state)
+
+        done = game_state["game_over"]
+
+        cur_planets_count = count_planets(game_state)
+
+        reward = cur_planets_count - self._last_planets_count
+        reward -= 0.01  # penalty to make them work faster
+        self._last_planets_count = cur_planets_count
+
+        return state, reward, done
+
+def my_id(state):
+    for player in state["players"]:
+        if player["itsme"]:
+            return player["id"]
+    return None
+
+def count_planets(game_state) -> int:
+    player_id = my_id(game_state)
+    planets = [
+        planet for planet in game_state["planets"] if planet["owner_id"] == player_id]
+    return len(planets)
+
+
+def to_game_action(planets, rl_action):
+    planets = {planet["id"]: planet for planet in planets}
+
+    source, target = action[0], action[1]
+    if source not in planet_ids or target not in planet_ids or source == target:
+        return NOOP
+
+    source = planet_ids[source]
+    target = planet_ids[target]
+
+    ships = source["ships"]
+    ship_ratios = action[2]
+
+    num_a = int(round(ships_ratio[0] * ships[0]))
+    num_b = int(round(ships_ratio[1] * ships[1]))
+    num_c = int(round(ships_ratio[2] * ships[2]))
+
+    if num_a == 0 and num_b == 0 and num_c == 0:
+        return NOOP
+    if num_a < 0 or num_b < 0 or num_c < 0:
+        print("ship ratios and ships:", ships_ratio, ships)
+    return f'send {source["id"]} {target["id"]} {num_a} {num_b} {num_c}'
+
+def parse_game_state(game_state):
+    pass
---
+++ ai/game_client.py
@@ -0,0 +1,53 @@
+import string
+import random
+import json
+import socket
+
+
+class RPSClient():
+
+    def __init__(self, addr=None, username=None, password=None, port=None):
+        if not username:
+            username = randomString()
+        if not password:
+            password = randomString()
+        self.username = username
+        self.password = password
+        if not addr:
+            addr = "localhost"
+        self.addr = addr
+        if not port:
+            port = 6000
+        self.port = port
+        self.io = self.new_socket()
+        self.login()
+
+    def new_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.addr, self.port))
+        return s.makefile('rw')
+
+    def login(self):
+        self.action('login {} {}'.format(self.username, self.password))
+
+    def reset(self):
+        self.io.close()
+        self.io = self.new_socket()
+        self.login()
+
+    def action(self, data):
+        self.io.write("{}\n".format(data))
+        self.io.flush()
+
+    def game_state(self):
+        while 1:
+            data = self.io.readline().strip()
+            if not data:
+                continue
+            elif data[0] == "{":
+                return json.loads(data)
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
